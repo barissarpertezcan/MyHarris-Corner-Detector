3482746889221db3f3877728eb811c119613f03a
barissarpertezcan <barissarper@gmail.com>
2022-08-09 13:49:34 +0300

    first commit

---
+++ harris_corner_test.py
@@ -0,0 +1,33 @@
+from my_harris_corner import *
+import cv2 as cv
+import numpy as np
+
+if __name__ == "__main__":
+    img = cv.imread("imgs/book1.png")
+    img = cv.resize(img, (500, 500), cv.INTER_AREA)
+    colored_img = img.copy()
+    colored_img2 = img.copy()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    score_matrix, corner_coords = MyHarrisCorner(gray, 3, 3, 0.04)
+
+    colored_img[score_matrix > 0.01 * score_matrix.max()]= [255, 0, 0]
+    # drawCorners(colored_img, corner_coords)
+
+    # opencv harris corner
+    gray = np.float32(gray)
+    dst = cv.cornerHarris(gray,3,3,0.04)
+    # dst = cv.dilate(dst,None)
+    colored_img2[dst>0.01*dst.max()]=[255, 0, 0]
+
+    org = (20, gray.shape[0] - 20)
+    colored_img = cv.putText(colored_img, 'My Harris', org, cv.FONT_HERSHEY_SIMPLEX, 
+                    1, (0, 0, 255), 2, cv.LINE_AA)
+
+    colored_img2 = cv.putText(colored_img2, 'Original Harris', org, cv.FONT_HERSHEY_SIMPLEX, 
+                1, (0, 0, 255), 2, cv.LINE_AA)
+
+    comparison = np.hstack((colored_img2, colored_img))
+    # cv.imwrite("comparison.png", comparison)
+    cv.imshow("comparison", comparison)
+    cv.waitKey(0)
---
+++ my_harris_corner.py
@@ -0,0 +1,64 @@
+import cv2 as cv
+from matplotlib.pyplot import draw
+import numpy as np
+
+def computeImageDerivative(img, ksize):
+    gx = cv.Sobel(img, cv.CV_32F, 1, 0, ksize)
+    gy = cv.Sobel(img, cv.CV_32F, 0, 1, ksize)
+    return gx, gy
+
+def getStructureTensor(coord, blocksize, gx, gy):
+    interval = int((blocksize - 1) / 2)
+
+    roi_x = gx[coord[1] - interval: coord[1] + interval + 1, coord[0] - interval: coord[0] + interval + 1]
+    roi_y = gy[coord[1] - interval: coord[1] + interval + 1, coord[0] - interval: coord[0] + interval + 1]
+    i_x_square = np.sum(np.power(roi_x, 2))
+    i_y_square = np.sum(np.power(roi_y, 2))
+    i_xy = np.sum(roi_x * roi_y)
+
+    M = np.array([[i_x_square, i_xy], [i_xy, i_y_square]])
+    return M
+
+def computeHarrisScore(M, k):
+    det_M = np.linalg.det(M)
+    tr_M = np.trace(M)
+    R = det_M - k * tr_M ** 2
+    return R
+
+def nonmaxsuppression(score_matrix, corner_coords, blocksize):
+    final_corner_coords = []
+
+    interval = int((blocksize - 1) / 2)
+
+    for columns, rows in corner_coords:
+        row, column = np.unravel_index(np.argmax(score_matrix[rows - interval: rows + interval + 1, columns - interval: columns + interval + 1]), (3, 3))
+        final_corner_coords.append((column + columns, row + rows))
+
+    final_corner_coords = list(set(final_corner_coords))
+    return final_corner_coords
+
+def drawCorners(img, coords, showimg = False):
+    for i, j in coords:
+        cv.circle(img, (j, i), 1, [255, 0, 0], -1)
+
+    if showimg:
+        cv.imshow("corners", img)
+        cv.waitKey(0)
+
+def MyHarrisCorner(img, blocksize, ksize, k):
+    # Applying Gaussian Blur to remove noises
+    img = cv.GaussianBlur(img, (blocksize, blocksize), 0)
+    gx, gy = computeImageDerivative(img, ksize)
+    score_matrix = np.zeros((img.shape[0], img.shape[1]))
+
+    interval = int((blocksize - 1) / 2)
+    for i in range(interval, img.shape[0] - interval):
+        for j in range(interval, img.shape[1] - interval):
+            M = getStructureTensor((j, i), blocksize, gx, gy)
+            R = computeHarrisScore(M, k)
+            score_matrix[i, j] = R
+
+    corner_coords = np.argwhere(score_matrix > 0.01 * score_matrix.max())
+    final_corner_coords = nonmaxsuppression(score_matrix, corner_coords, blocksize)
+    return score_matrix, final_corner_coords
+    
